15c0fe56d85cdd0d03309591b96bd4ab34c8d909
lbowen3 <144710267+lbowen3@users.noreply.github.com>
2024-03-15 19:30:18 +0000

    Started Code

--- main.hpp
+++ main.hpp
@@ -1,8 +1,34 @@
+#include <iostream>
+using namespace std;
+
 
 //********************
 // Declare Function Prototypes
 //********************
 
+int getTwoValues(int, int);
+int getNextPrime(int);
+int getPrevPrime(int);
+
+
 // ******************************
 // Implement all your functions here
 // ******************************
+
+int getTwoValues(int begin, int end)
+{
+    do
+    {
+        cout << "Enter beginning and end values: ";
+        cin >> begin >> end;
+    } while (begin >= end);
+    return begin, end;
+}
+int getNextPrime(int num)
+{
+
+}
+int getPrevPrime(int num)
+{
+
+}
